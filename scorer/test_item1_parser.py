885bf3c0abef100998d0baa65c6f76d1795b3ef3
Wabognie <tiphaine.casy@hotmail.fr>
2021-04-25 15:02:31 +0200

    update

--- scorer/test_item1_parser.py
+++ scorer/test_item1_parser.py
@@ -1,34 +1,3 @@
-
-
-def run(test_dataset):
-    """
-    Run test
-    """
-
-    ## imortation
-    import pandas as pd
-    from . import item1_parser
-
-    score = 0
-    ## load test dataset
-    df = pd.read_csv(test_dataset)
-    for index, row in df.iterrows():
-
-        #-> loop over each entry
-        title = row['TITLE']
-        abstract = row['ABSTRACT']
-        present = row['ITEM1']
-
-        ## run item1 parser
-        match = item1_parser.first_try(title, abstract)
-
-        ## comparre match & score
-        if str(match) == str(present) :
-            score += 1
-        return score
-
-
-
 def test_simple_guess(test_dataset):
     """
     """
